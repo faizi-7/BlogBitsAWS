77c415b5bda258d55a2af129e67e9c19ab57369c
faizi-7 <faiziqbal733@gmail.com>
2023-07-06 12:27:31 +0530

    first commit

---
+++ client/src/Pages/Write/Write.jsx
@@ -0,0 +1,80 @@
+import React, { useContext, useState } from "react";
+import "./Write.scss";
+import { BiSolidImageAdd } from "react-icons/bi";
+import { Context } from "../../context/Context";
+import axios from "axios";
+
+const Write = () => {
+  const [title, setTitle] = useState("");
+  const [desc, setDesc] = useState("");
+  const [file, setFile] = useState(null);
+  const { user } = useContext(Context);
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const newPost = {
+      username: user.username,
+      title,
+      desc,
+    };
+    if (file) {
+      const data = new FormData();
+      const filename = Date.now() + file.name;
+      data.append("name", filename);
+      data.append("file", file);
+      newPost.photo = filename;
+      try {
+        await axios.post("/api/upload", data);
+      } catch (error) {}
+    }
+    try {
+      const res = await axios.post("/api/posts", newPost);
+      window.location.replace("/post/" + res.data._id);
+    } catch (error) {}
+  };
+
+  return (
+    <div className="write">
+      <div className="writeContainer">
+        {file && (
+          <img src={URL.createObjectURL(file)} alt="" className="writeImg" />
+        )}
+        <form className="writeForm" onSubmit={handleSubmit}>
+          <div className="writeFormGroup">
+            <label htmlFor="fileInput" className="inputImage">
+              <BiSolidImageAdd />
+            </label>
+            <input
+              id="fileInput"
+              type="file"
+              style={{ display: "none" }}
+              onChange={(e) => setFile(e.target.files[0])}
+            />
+            <input
+              className="writeInput"
+              placeholder="Title"
+              type="text"
+              autoFocus={true}
+              onChange={e=> setTitle(e.target.value)}
+              value={title}
+            />
+          </div>
+          <div className="writeFormGroup">
+            <textarea
+              className="writeInput writeText"
+              placeholder="Tell your story..."
+              type="text"
+              autoFocus={true}
+              onChange={e=> setDesc(e.target.value)}
+              value={desc}
+            />
+          </div>
+          <button className="writeSubmit" type="submit">
+            Publish
+          </button>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default Write;
---
+++ client/src/components/Post/Post.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import "./Post.scss";
+import { Link} from 'react-router-dom'
+
+const Post = ({ post }) => {
+  const PF = "http://localhost:5000/images/"
+  return (
+    <div className="post">
+      {post.photo && <img className="postImg" src={PF + post.photo} />}
+      <div className="postInfo">
+        <div className="postCats">
+          {post.categories.map((category) => (
+            <span className="postCat">{category.name}</span>
+          ))}
+        </div>
+        <Link to={`/post/${post._id}`} className="link">
+          <div className="postTitle">{post.title}</div>
+        </Link>
+        <span className="postDate">
+          {new Date(post.createdAt).toDateString()}
+        </span>
+      </div>
+      <p className="postDesc">{post.desc}</p>
+    </div>
+  );
+};
+
+export default Post;
